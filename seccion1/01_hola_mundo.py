f78f659093640cd3bb151bb3eeb899c06fd2f374
isabuitrago1223 <buitragoisa1223@gmail.com>
2026-04-21 16:35:18 -0500

    LAB 01 - Hola Mundo con uso de print()

---
+++ seccion1/01_hola_mundo.py
@@ -0,0 +1,7 @@
+# Este programa utiliza la función print() para mostrar mensajes en pantalla
+
+# Imprime el mensaje "¡Hola, Mundo!"
+print("¡Hola, Mundo!")
+
+# Imprime mi nombre
+print("Isabella")
